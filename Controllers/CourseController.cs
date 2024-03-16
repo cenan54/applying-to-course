9674d8f9abc3edd85bd668a092548f0a69757690
Mustafa Cenan <54mustafacenan54@gmail.com>
2024-03-17 00:42:40 +0300

    Course controller eklendi

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BtkAkademi.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
